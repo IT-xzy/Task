9a6a6152b4339b803db7981f99e0247709e37064
java <ptteng@users.noreply.github.com>
2018-03-23 22:27:35 +0800

    Creat yuanlei

---
+++ Java/task_1/src/main/java/com/jnshu/pojo/student.java
@@ -0,0 +1,57 @@
+package com.jnshu.pojo;
+
+public class student {
+    private int id;
+    private String name;
+    private long QQ;
+    private  String Online_ID;
+    private  String time_of_enrollment;
+    private  String graduated_university;
+    private  String daily_link;
+    private  String declaration;
+    private  String coaching_senior;
+//    private  String hear_from;
+
+
+
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+
+
+    public long getQQ(){
+        return QQ;
+    }
+
+   public String getOnline_ID(){
+        return Online_ID;
+   }
+   public String getTime_of_enrollment(){
+        return time_of_enrollment;
+   }
+
+    public String getGraduated_university() {
+        return graduated_university;
+    }
+
+    public String getDaily_link() {
+        return daily_link;
+    }
+
+    public String getDeclaration() {
+        return declaration;
+    }
+
+    public String getCoaching_senior() {
+        return coaching_senior;
+    }
+
+//    public String getHear_from() {
+//        return hear_from;
+//    }
+
+
+}
