fdb7690d582eda175634bd274671ca34aeef9eeb
Administrator <1097856398@qq.com>
2018-03-23 11:49:58 +0800

    css和js任务

--- Web/wangjiaoyan/JS/js-1/js-1.js
+++ Web/wangjiaoyan/JS/js-1/js-1.js
@@ -3,6 +3,7 @@
 // //  */
 var a;
 function start() {
+    clearInterval(a);
     // 选取随机数
     function GetRandomNum(Min,Max)
     {
@@ -16,14 +17,14 @@ function start() {
         var num2 = GetRandomNum(0,8);
         var num3 = GetRandomNum(0,8);
         if (num1!==num2&&num2!=num3&&num1!==num3){
-        break;
+            break;
         }
     }
     // 获取dom节点
     var choose=document.getElementsByClassName("box");
     for (var j=0;j<choose.length;j++){
-    choose[j].style.background="orange";
-}
+        choose[j].style.background="orange";
+    }
     // 随机颜色
     function bg1(){
         var r=Math.floor(Math.random()*256);
@@ -32,15 +33,15 @@ function start() {
         return "rgb("+r+','+g+','+b+")";
     }
     // 给随机格子赋随机颜色
-        choose[num1].style.backgroundColor = bg1();
-        choose[num2].style.backgroundColor = bg1();
-        choose[num3].style.backgroundColor = bg1();
+    choose[num1].style.backgroundColor = bg1();
+    choose[num2].style.backgroundColor = bg1();
+    choose[num3].style.backgroundColor = bg1();
 
-    a = setTimeout("start()",1000);
+    a = window.setInterval("start()",1000);
+
 }
-
 function end() {
-    clearTimeout(a);
+    clearInterval(a);
     var choose = document.getElementsByClassName("box");
     for (var x = 0; x < choose.length; x++) {
         choose[x].style.backgroundColor = "orange";
