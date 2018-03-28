6fd11ef9a8865c41b18ac4d89a6279a77fac064d
Pumpking <gn131131@sina.com>
2018-03-28 17:29:58 +0800

    js6
    js task 6 update

--- Web/guannan/JSproject06/script/app.js
+++ Web/guannan/JSproject06/script/app.js
@@ -1,13 +1,23 @@
-//创建一个app，注入ui-router模块
-var app = angular.module('myApp', ['ui.router'])
-app.config(["$stateProvider", function($stateProvider) {
+'use strict'
+
+//创建一个app，注入模块+++
+var app = angular.module('myApp', ['ui.router', 'ui.bootstrap', 'ngAnimate']);
+//路由跳转+++
+app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when('', 'login');
+    $urlRouterProvider.when('/', '/login');
+
     $stateProvider
-    .state("list", {
-        url: '/list',
-        templateUrl: './article-list.html'
+    .state("login", {//登陆页+++
+        url: '/login',
+        templateUrl: 'views/login.html'
     })
-    .state("detail", {
-        url: '/detail',
-        templateUrl: './article-detail.html'
+    .state("backstage", {//后台页+++
+        url: '/backstage',
+        templateUrl: 'views/backstage.html'
+    })
+    .state("backstage.list", {//Article列表页+++
+        url: '/article/list',
+        templateUrl: 'views/article-list.html'
     })
 }]);
