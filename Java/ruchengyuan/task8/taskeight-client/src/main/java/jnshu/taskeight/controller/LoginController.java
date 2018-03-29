e9a48a72fb6e440ddf49e9841b9a12640550e9d8
qiyueliuhuo <744336687@qq.com>
2018-03-29 23:40:41 +0800

    Add files via upload

---
+++ Java/ruchengyuan/task8/taskeight-client/src/main/java/jnshu/taskeight/controller/LoginController.java
@@ -0,0 +1,87 @@
+package jnshu.taskeight.controller;
+
+import jnshu.taskeight.model.Student;
+
+import jnshu.taskeight.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * 处理登陆后的业务
+ * @author: Administrator
+ * @date: 2017-10-21
+ * @Time: 上午 12:43
+ * Description:
+ **/
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private StudentService studentService;
+
+    private static Logger loggerCommCon = LoggerFactory.getLogger(LoginController.class);
+
+
+    //查询所有学生信息JSON格式
+    @RequestMapping(value = "/a/u/student/all", method = RequestMethod.GET)
+    public String studentAll(HttpServletRequest request, HttpServletResponse response, Model model) {
+        try {
+            List<Student> students = studentService.listStudentTable();
+            loggerCommCon.info("students number: " + students.size());
+            model.addAttribute("students", students);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            loggerCommCon.error(t.getMessage());
+            return "/common/errorJson";
+        }
+        return "student/studyAll";
+    }
+
+
+    //表格界面
+    @RequestMapping(value = "/a/u/student/table", method = RequestMethod.GET)
+    public String studentTable(HttpServletRequest request, HttpServletResponse response, Model model) {
+        try {
+            List<Student> studentTable = studentService.listStudentTable();
+            loggerCommCon.info("studentTable : " + studentTable.size());
+            model.addAttribute("studentTable", studentTable);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            loggerCommCon.error(t.getMessage());
+            return "/common/errorJson";
+        }
+        return "student/studentTable";
+    }
+
+
+    //上传图片
+    @RequestMapping(value = "/a/u/student/pictrue", method = RequestMethod.GET)
+    public String studentPicture(HttpServletRequest request, HttpServletResponse response, Model model) {
+
+        return "task7/picture";
+    }
+
+
+    @RequestMapping(value = "/a/u/student/information", method = RequestMethod.GET)
+    public String studentInformation(HttpServletRequest request, HttpServletResponse response, Model model) {
+
+        return "task7/phoneAndEmail";
+    }
+
+
+}
+
+
+
+
