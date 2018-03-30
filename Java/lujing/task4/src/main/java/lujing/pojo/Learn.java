26b6f0915521346d0b4b5bcaca8ee5c85620aa06
zm <33777919+y5284037@users.noreply.github.com>
2018-03-30 20:02:13 +0800

    Add files via upload

---
+++ Java/lujing/task4/src/main/java/lujing/pojo/Learn.java
@@ -0,0 +1,52 @@
+package lujing.pojo;
+
+public class Learn {
+    private Integer id;
+
+    private String message;
+
+    private String picturePath;
+
+    public Learn(Integer id, String message, String picturePath) {
+        this.id = id;
+        this.message = message;
+        this.picturePath = picturePath;
+    }
+
+    public Learn() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message == null ? null : message.trim();
+    }
+
+    public String getPicturePath() {
+        return picturePath;
+    }
+
+    public void setPicturePath(String picturePath) {
+        this.picturePath = picturePath == null ? null : picturePath.trim();
+    }
+    
+    @Override
+    public String toString() {
+        return "Learn{" +
+                "id=" + id +
+                ", message='" + message + '\'' +
+                ", picturePath='" + picturePath + '\'' +
+                '}';
+    }
+}
