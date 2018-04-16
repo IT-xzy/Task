d293b306b9cd3dd5925563f74fefb7e1a43d0e01
f056917 <33644849+f056917@users.noreply.github.com>
2018-04-16 10:56:36 +0800

    Add files via upload

--- Web/fengyachao/task/JS/JS2/js/task.js
+++ Web/fengyachao/task/JS/JS2/js/task.js
@@ -2,5 +2,9 @@ function back() {
     window.location.href = "set.html"
 }
 function home() {
+    sessionStorage.removeItem('dieNum');
+    sessionStorage.removeItem('dieList');
+    sessionStorage.removeItem('K');
+    sessionStorage.removeItem('P');
     window.location.href = "home.html"
 }
