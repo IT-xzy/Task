8c7f267f02d0b49be3ef03b5c82b1cfce47c5117
cy3107 <137334398@qq.com>
2018-04-27 09:30:54 +0800

    task4
    task4

---
+++ Java/caoyue/task4/task4/src/main/java/com/POJO/GreatStudent.java
@@ -0,0 +1,56 @@
+package com.POJO;
+
+/**
+ * @author: 曹樾
+ * @program: task4
+ * @description: 优秀学员
+ * @create: 2018/4/24 下午4:05
+ */
+
+public class GreatStudent {
+    private Integer ID;
+    private String name;
+    private String picture;
+    private String profession;
+    private String introduce;
+    
+    public Integer getID() {
+        return ID;
+    }
+    
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getPicture() {
+        return picture;
+    }
+    
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+    
+    public String getProfession() {
+        return profession;
+    }
+    
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+    
+    public String getIntroduce() {
+        return introduce;
+    }
+    
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+}
