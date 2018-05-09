9fb19b713a1b10318f091d05eebcf6ae1fdb2a19
leon <len_yzszsl@163.com>
2018-05-09 11:21:51 +0800

    提交任务四

---
+++ Java/louwenbin/task4-3/src/main/java/com/utils/tag/DemoViewTag.java
@@ -0,0 +1,35 @@
+package com.utils.tag;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author louisliao
+ */
+public class DemoViewTag extends TagSupport {
+    private long time;
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public int doEndTag() throws JspException  {
+        //long time = System.currentTimeMillis();
+        SimpleDateFormat matter = new SimpleDateFormat("现在时间:yyyy年MM月dd日E HH时mm分ss秒");
+        Date date = new Date(time);
+        JspWriter writer = pageContext.getOut();
+        try {
+            writer.print(matter.format(date));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return super.EVAL_PAGE;
+    }
+}
