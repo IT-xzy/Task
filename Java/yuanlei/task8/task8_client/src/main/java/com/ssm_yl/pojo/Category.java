0c6fb0e8eb2b50e5851c2c3377e8a0791fc1bef0
yuanlei <37693972+532974662@users.noreply.github.com>
2018-05-23 11:41:26 +0800

    Add files via upload

---
+++ Java/yuanlei/task8/task8_client/src/main/java/com/ssm_yl/pojo/Category.java
@@ -0,0 +1,20 @@
+package com.ssm_yl.pojo;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+    private int id;
+    private String name;
+    private  int age;
+    public int getId(){return id;}
+    public  void setId(int id){this.id = id;}
+    public  String  getName(){return name;}
+    public void setName(String name){this.name = name;}
+    public int getAge(){return age;}
+    public void setAge(int age){this.age = age;}
+
+    @Override
+    public String toString(){
+        return "Category{"+"id="+id+",name="+name+",age="+age+"}";
+    }
+}
