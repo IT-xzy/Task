b2b350d9567232399e988eaf330c12d2cb255a56
jin4329 <37407032+jin4329@users.noreply.github.com>
2018-06-11 11:52:49 +0800

    Add files via upload

---
+++ Java/jinliangjin/task6Redies/src/main/java/com/ptt/dao/ILoginGroup.java
@@ -0,0 +1,12 @@
+package com.ptt.dao;
+
+/**
+ * @ClassName: ILoginGroup
+ * @Description:
+ * @Author: Jin
+ * @CreateDate: 2018/6/1 11:15
+ * @Version: 1.0
+ */
+public interface ILoginGroup {
+    //用于注册时校验分组
+}
