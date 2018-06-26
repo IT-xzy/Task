75e140167de6b9cfc1dbf2ec30f823588b5a8f81
java <ptteng@users.noreply.github.com>
2018-06-27 07:13:46 +0800

    Delete SignMapper.java

--- Java/zzhTask1/SpringServer/src/main/Java/com/Dao/SignMapper.java
+++
@@ -1,9 +0,0 @@
-package com.Dao;
-public interface SignMapper {
-    public Sign findUserById(int id);
-    public Sign findUserByName(String name);
-    public int insertUser(Sign user);
-    public int deleteUserById(int id);
-    public int updateUserById(Sign user);
-    public int deleteUserByName(String name);
-}
