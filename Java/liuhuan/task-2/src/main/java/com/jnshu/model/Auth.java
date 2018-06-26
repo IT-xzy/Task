625574d3ac6f7ebe68dd44e4709cb7d678864ce4
weiba <lh1738289913@gmail.com>
2018-06-26 20:23:15 +0800

    Add files via upload
    添加任务二

---
+++ Java/liuhuan/task-2/src/main/java/com/jnshu/model/Auth.java
@@ -0,0 +1,25 @@
+package com.jnshu.model;
+
+/* 登陆 */
+public class Auth {
+    //登陆用户名
+    private String username;
+    //登陆密码
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
