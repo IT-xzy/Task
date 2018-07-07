93e694bb09d9ab12ab613e1e4c4335f1040f2ad6
jin4329 <37407032+jin4329@users.noreply.github.com>
2018-07-07 18:05:25 +0800

    Add files via upload

---
+++ Java/jinliangjin/task9/src/main/java/com/tuscany/demo/server/ISum.java
@@ -0,0 +1,12 @@
+package com.tuscany.demo.server;
+
+/**
+ * @ClassName: ISum
+ * @Description:
+ * @Author: Jin
+ * @CreateDate: 2018/7/4 11:07
+ * @Version: 1.0
+ */
+public interface ISum {
+    Double add(Double a, Double b);
+}
