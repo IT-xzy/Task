3d4b43d411234e8ec640e7b0b69c864f9bc0a214
jnuff-u <137416943@qq.com>
2018-07-25 15:42:13 +0800

    Add files via upload

---
+++ Java/zhangqiang/task9/tuscanyClien/src/main/java/com/opt/controller/ServletListener.java
@@ -0,0 +1,22 @@
+package com.opt.controller;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+
+public class ServletListener implements ServletContextAttributeListener {
+
+    @Override
+    public void attributeAdded(ServletContextAttributeEvent scae) {
+
+    }
+
+    @Override
+    public void attributeRemoved(ServletContextAttributeEvent scae) {
+
+    }
+
+    @Override
+    public void attributeReplaced(ServletContextAttributeEvent scae) {
+
+    }
+}
