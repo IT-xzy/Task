10d0bd9de2b70cdb57f4db0b5badc7d679a2a51e
willming24 <38829340+willming24@users.noreply.github.com>
2018-08-10 15:40:38 +0800

    Add files via upload

---
+++ Java/liweiming/task8/rmi-service-b/src/main/java/com/jnshu/Rmi/RmiTest.java
@@ -0,0 +1,16 @@
+package com.jnshu.Rmi;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @program: Tiles
+ * @description:
+ * @author: Mr.Lee
+ * @create: 2018-08-08 22:48
+ **/
+public class RmiTest {
+    public static void main(String args[]){
+        new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println("服务端RMI启动 端口号");
+    }
+}
