6dc34c9fceba40e81ec8ffa72fd47e6e90418a1d
willming24 <38829340+willming24@users.noreply.github.com>
2018-08-21 20:25:55 +0800

    Add files via upload

---
+++ Java/liweiming/task9/src/main/java/service/subtractInterface.java
@@ -0,0 +1,6 @@
+package service;
+
+
+public interface subtractInterface {
+    double subtract(double n1,double n2);
+}
---
+++ Java/liweiming/task9/src/main/java/servieImpl/add.java
@@ -0,0 +1,11 @@
+package servieImpl;
+
+import service.addInterface;
+
+
+public class add implements addInterface {
+    public double add(double n1,double n2){
+        return n1+n2;
+
+    }
+}
