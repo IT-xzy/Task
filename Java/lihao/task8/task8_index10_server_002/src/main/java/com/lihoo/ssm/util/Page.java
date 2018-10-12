e3728b39da40232e4757a73d19b00a097ca81e2a
li hao <whalesong939@163.com>
2018-10-12 21:13:41 +0800

    提交任务8

---
+++ Java/lihao/task8/task8_index10_server_002/src/main/java/com/lihoo/ssm/util/Page.java
@@ -0,0 +1,51 @@
+package com.lihoo.ssm.util;
+
+/**
+ * @author lihoo
+ * @Title: Page
+ * @ProjectName Spring_SpringMVC_Mybatis_OG_1
+ * @Description: TODO
+ * @date 2018/8/7-8:56
+ */
+
+@SuppressWarnings("unused")
+public class Page {
+    int start = 0;
+    int count = 5;
+    int last = 0;
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getLast() {
+        return last;
+    }
+
+    public void setLast(int last) {
+        this.last = last;
+    }
+
+    public void caculateLast(int total) {
+//        假设总数是50，是能够被5整除的，那么最后一页的开始就是45；
+        if (0 == total % count) {
+            last = total - count;
+//            假设总数是51，不能够被5整除，那么最后一页开始就是50；
+        } else {
+            last = total - total % count;
+        }
+    }
+
+}
