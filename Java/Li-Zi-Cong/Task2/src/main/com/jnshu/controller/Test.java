6810440b762226419f1e926ea314ffb56a1f07c7
lizicong <845634109@qq.com>
2018-12-28 04:34:54 +0800

    done434

--- Java/Li-Zi-Cong/Task2/src/main/com/jnshu/controller/Test.java
+++
@@ -1,9 +0,0 @@
-package main.com.jnshu.controller;
-
-public class Test {
-    public static void main(String[] args) {
-        float a= 10;
-        System.out.println(a%6);
-        //%HOMEDRIVE%%HOMEPATH%
-    }
-}
