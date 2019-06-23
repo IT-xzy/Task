a4e7500c16ff1298a4ee7186dc15df2379df20db
baidongn <50824583+baidongn@users.noreply.github.com>
2019-06-23 14:34:19 +0800

    Add files via upload

---
+++ task05/src/test/java/StudentTest.java
@@ -0,0 +1,25 @@
+import com.jnshu.model.Student;
+import com.jnshu.service.StudentService;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@ContextConfiguration("classpath:spring/applicationContext.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class StudentTest {
+
+    @Autowired
+    private StudentService studentService;
+    Logger logger = Logger.getLogger(BannerTest.class);
+
+    @Test
+    public void select(){
+        List<Student> student = studentService.selectAll();
+        logger.info(student);
+    }
+}
