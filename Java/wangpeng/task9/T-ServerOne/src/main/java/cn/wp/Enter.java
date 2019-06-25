e328307aa1fe35eab3536b9cf9e73dfd950c80e0
老王 <49674862+wangpengwpw@users.noreply.github.com>
2019-06-25 15:38:48 +0800

    Add files via upload

---
+++ Java/wangpeng/task9/T-ServerOne/src/main/java/cn/wp/Enter.java
@@ -0,0 +1,21 @@
+package cn.wp;
+
+import org.apache.tuscany.sca.node.Node;
+import org.apache.tuscany.sca.node.NodeFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/** 启动服务类 */
+public class Enter implements ServletContextListener {
+
+  @Override
+  public void contextInitialized(ServletContextEvent servletContextEvent) {
+    Node node = NodeFactory.newInstance().createNode("Tuscany.composite");
+    node.start();
+    System.out.println("服务1已启动!");
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent servletContextEvent) {}
+}
